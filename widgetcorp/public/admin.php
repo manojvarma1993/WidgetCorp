d343d557423217d296fd107a6a90d031b7318991
manoj <manojvarmagottumukkala@gmail.com>
2017-11-16 23:27:38 -0600

    this is secod commit

--- widgetcorp/public/admin.php
+++ widgetcorp/public/admin.php
@@ -9,6 +9,7 @@
   <div id="navigation">
     &nbsp;
   </div>
+  <!-- this is commit code -->
   <div id="page">
     <h2>Admin Menu</h2>
     <p>Welcome to the admin area, <?php echo htmlentities($_SESSION["username"]); ?>.</p>
